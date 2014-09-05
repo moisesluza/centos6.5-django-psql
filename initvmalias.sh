2072e099033446787facfcf8f4a369521cff3ebe
Moises Luza <moises.luza@gmail.com>
2014-09-04 23:43:38 -0700

    Adding command to init django webserver

--- initvmalias.sh
+++ initvmalias.sh
@@ -1,5 +1,7 @@
+#shortcut to initialize vagrant
 cd ~/Documents/Emar/centos6.5-django-psql
 vagrant up
 vagrant ssh
 source ~/mycart/mycartvenv/bin/activate
 cd ~/mycart/mycartapp
+./manage.py runserver 0.0.0.0:3000
